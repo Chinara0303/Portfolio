ca214eeb3b5a3cb542015acdb7cc8927497b987c
Chinara0303 <chinaraei@code.edu.az>
2022-11-17 18:32:12 +0400

    almost done

--- src/data.js
+++ src/data.js
@@ -4,28 +4,14 @@ export const DataWebSites = [
     title: "Ecoshop",
     src:"../assets/ECOSHOPimg.png",
     github:"https://github.com/Chinara0303/Echoshop",
-    // weblink:"https://echoshopp.netlify.app/index.html"
+    weblink:"https://echoshopp.netlify.app/index.html"
   },
   {
-    id:1,
-    title: "Ecoshop",
-    src:"../assets/ECOSHOPimg.png",
-    github:"https://github.com/Chinara0303/Echoshop",
-    // weblink:"https://echoshopp.netlify.app/index.html"
-  },
-  {
-    id:1,
-    title: "Ecoshop",
-    src:"../assets/ECOSHOPimg.png",
-    github:"https://github.com/Chinara0303/Echoshop",
-    // weblink:"https://echoshopp.netlify.app/index.html"
-  },
-  {
-    id:1,
-    title: "Ecoshop",
-    src:"../assets/ECOSHOPimg.png",
-    github:"https://github.com/Chinara0303/Echoshop",
-    // weblink:"https://echoshopp.netlify.app/index.html"
+    id:2,
+    title: "Nest",
+    src:"../assets/Nestimg.png",
+    github:"https://github.com/Chinara0303/Nest",
+    weblink:"https://nestapp.netlify.app/"
   }
 
 ]
