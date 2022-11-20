1c4ab2f75bde8f9ed1010f53c5a282568e32f24c
Chinara0303 <chinaraei@code.edu.az>
2022-11-20 15:41:35 +0400

    almost done

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,12 +3,18 @@ import "./intro.scss"
 export default function Intro() {
   return (
     <div className='intro' id='intro'>
+      <i className="fa-solid fa-moon moon"></i>
+      <i className="fa-solid fa-star solidstar1"></i>
+      <i className="fa-regular fa-star regularstar1"></i>
+      <i className="fa-solid fa-star solidstar2"></i>
+      <i className="fa-regular fa-star regularstar2"></i>
       <div className='left'>
         <div className='imgContainer'>
           <img src='../assets/China.png' alt='' />
         </div>
       </div>
       <div className='rigth'>
+      
         <div className='wrapper'>
           <h2>Hi, There I'm</h2>
           <h1>Chinara Ibadova</h1>
--- src/data.js
+++ src/data.js
@@ -12,20 +12,6 @@ export const dataWebSites = [
     src:"../assets/Nestimg.png",
     github:"https://github.com/Chinara0303/Nest",
     weblink:"https://nestapp.netlify.app/"
-  },
-  {
-    id:1,
-    title: "Ecoshop",
-    src:"../assets/ECOSHOPimg.png",
-    github:"https://github.com/Chinara0303/Echoshop",
-    weblink:"https://echoshopp.netlify.app/index.html"
-  },
-  {
-    id:2,
-    title: "Nest",
-    src:"../assets/Nestimg.png",
-    github:"https://github.com/Chinara0303/Nest",
-    weblink:"https://nestapp.netlify.app/"
   }
 ]
 
