f4a223e73cc11b1720e9d0787a5e4bd3736a34e2
Chinara0303 <chinaraei@code.edu.az>
2022-11-30 15:21:13 +0400

    almost done

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -5,9 +5,6 @@ export default function Intro() {
     <div className='intro' id='intro'>
       <i className="fa-solid fa-moon moon"></i>
       <i className="fa-solid fa-star solidstar1"></i>
-      <i className="fa-regular fa-star regularstar1"></i>
-      <i className="fa-solid fa-star solidstar2"></i>
-      <i className="fa-regular fa-star regularstar2"></i>
       <div className='left'>
         <div className='imgContainer'>
           <img src='../assets/China.png' alt='' />
