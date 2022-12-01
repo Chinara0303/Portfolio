70f4e5c816609890bcef7e1438df5c8f74ce6862
Chinara0303 <chinaraei@code.edu.az>
2022-12-01 15:25:33 +0400

    added preloader

--- src/App.js
+++ src/App.js
@@ -5,20 +5,37 @@ import Skills from "./components/Skills/Skills"
 import Intro from "./components/Intro/Intro"
 import { useState } from 'react';
 import Menu from './components/Menu/Menu';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
- const [menuOpen,SetmenuOpen] = useState(false)
-
+  const [menuOpen, SetmenuOpen] = useState(false)
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById("spinner");
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none";
+      setLoading(false);
+    }, 2000);
+  }
   return (
-    <div className='App'>
-        <Topbar menuOpen={menuOpen} SetmenuOpen={SetmenuOpen}/>
-        <Menu menuOpen={menuOpen} SetmenuOpen={SetmenuOpen}/>
+    !loading && (
+      <div className='App'>
+        <Topbar menuOpen={menuOpen} SetmenuOpen={SetmenuOpen} />
+        <Menu menuOpen={menuOpen} SetmenuOpen={SetmenuOpen} />
         <div className="sections">
-          <Intro />
+          <BrowserRouter>
+            <Routes>
+              <Route exact path='/' element={<Intro />} />
+              <Route path='/skills' element={<Skills />} />
+              <Route path='/works' element={<Works />} />
+            </Routes>
+          </BrowserRouter>
           <Skills/>
           <Works/>
         </div>
-    </div>
+      </div>
+    )
+
   );
 }
 
